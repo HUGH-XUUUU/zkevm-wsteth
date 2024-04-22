77fd559d71a543763eaeae48ca446a183f2f3780
HUGH-XUUUU <2325249150@qq.com>
2024-04-22 19:09:53 +0800

    add test and Create3Factory contract

--- src/WstETHBridgeL2.sol
+++ src/WstETHBridgeL2.sol
@@ -3,14 +3,11 @@ pragma solidity 0.8.17;
 
 import {IERC20} from "oz/token/ERC20/IERC20.sol";
 import {UUPSUpgradeable} from "upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {AccessControlDefaultAdminRulesUpgradeable} from
-  "upgradeable/access/AccessControlDefaultAdminRulesUpgradeable.sol";
-import {PausableUpgradeable} from
-  "upgradeable/security/PausableUpgradeable.sol";
+import {AccessControlDefaultAdminRulesUpgradeable} from "upgradeable/access/AccessControlDefaultAdminRulesUpgradeable.sol";
+import {PausableUpgradeable} from "upgradeable/security/PausableUpgradeable.sol";
 
 import {IPolygonZkEVMBridge} from "./interfaces/IPolygonZkEVMBridge.sol";
-import {PolygonERC20BridgeLibUpgradeable} from
-  "./base/PolygonERC20BridgeLibUpgradeable.sol";
+import {PolygonERC20BridgeLibUpgradeable} from "./base/PolygonERC20BridgeLibUpgradeable.sol";
 import {WstETHWrapped} from "./WstETHWrapped.sol";
 
 /**
@@ -19,111 +16,106 @@ import {WstETHWrapped} from "./WstETHWrapped.sol";
  * @notice Main smart contract to bridge wstETH from Polygon zkEVM to Ethereum
  */
 contract WstETHBridgeL2 is
-  UUPSUpgradeable,
-  AccessControlDefaultAdminRulesUpgradeable,
-  PausableUpgradeable,
-  PolygonERC20BridgeLibUpgradeable
+    UUPSUpgradeable,
+    AccessControlDefaultAdminRulesUpgradeable,
+    PausableUpgradeable,
+    PolygonERC20BridgeLibUpgradeable
 {
-  /// @notice Role identifiers
-  bytes32 public constant EMERGENCY_ROLE = keccak256("EMERGENCY_ROLE");
+    /// @notice Role identifiers
+    bytes32 public constant EMERGENCY_ROLE = keccak256("EMERGENCY_ROLE");
 
-  /// @notice wstETH contract on Polygon zkEVM
-  WstETHWrapped public wrappedTokenAddress;
+    /// @notice wstETH contract on Polygon zkEVM
+    WstETHWrapped public wrappedTokenAddress;
 
-  /// @notice wstETH contract on Ethereum mainnet
-  IERC20 public originTokenAddress;
+    /// @notice wstETH contract on Ethereum mainnet
+    IERC20 public originTokenAddress;
 
-  /// @notice wstETH origin from mainnet = 0; if from zkEVM then 1
-  uint32 public originTokenNetwork = 0;
+    /// @notice wstETH origin from mainnet = 0; if from zkEVM then 1
+    uint32 public originTokenNetwork = 0;
 
-  /// @notice Disable initializer on deploy
-  constructor() {
-    _disableInitializers();
-  }
+    /// @notice Disable initializer on deploy
+    constructor() {
+        _disableInitializers();
+    }
 
-  /**
-   * @notice WstETHBridgeL2 initializer
-   * @dev This initializer should be called via UUPSProxy constructor
-   * @param _adminAddress The contract owner
-   * @param _emergencyRoleAddress The emergency role address
-   * @param _polygonZkEVMBridge The Polygon zkEVM bridge address
-   * @param _counterpartContract The contract address of L1Escrow
-   * @param _counterpartNetwork ID of Ethereum mainnet on the Polygon zkEVM
-   * bridge
-   */
-  function initialize(
-    address _adminAddress,
-    address _emergencyRoleAddress,
-    WstETHWrapped _wrappedTokenAddress,
-    IERC20 _originTokenAddress,
-    IPolygonZkEVMBridge _polygonZkEVMBridge,
-    address _counterpartContract,
-    uint32 _counterpartNetwork
-  ) public initializer {
-    __AccessControlDefaultAdminRules_init(3 days, _adminAddress);
-    __UUPSUpgradeable_init();
-    __Pausable_init();
-    __PolygonERC20BridgeLib_init(
-      _polygonZkEVMBridge, _counterpartContract, _counterpartNetwork
-    );
+    /**
+     * @notice WstETHBridgeL2 initializer
+     * @dev This initializer should be called via UUPSProxy constructor
+     * @param _adminAddress The contract owner
+     * @param _emergencyRoleAddress The emergency role address
+     * @param _polygonZkEVMBridge The Polygon zkEVM bridge address
+     * @param _counterpartContract The contract address of L1Escrow
+     * @param _counterpartNetwork ID of Ethereum mainnet on the Polygon zkEVM
+     * bridge
+     */
+    function initialize(
+        address _adminAddress,
+        address _emergencyRoleAddress,
+        WstETHWrapped _wrappedTokenAddress,
+        IERC20 _originTokenAddress,
+        IPolygonZkEVMBridge _polygonZkEVMBridge,
+        address _counterpartContract,
+        uint32 _counterpartNetwork
+    ) public initializer {
+        __AccessControlDefaultAdminRules_init(3 days, _adminAddress);
+        __UUPSUpgradeable_init();
+        __Pausable_init();
+        __PolygonERC20BridgeLib_init(
+            _polygonZkEVMBridge,
+            _counterpartContract,
+            _counterpartNetwork
+        );
 
-    _grantRole(EMERGENCY_ROLE, _emergencyRoleAddress);
+        _grantRole(EMERGENCY_ROLE, _emergencyRoleAddress);
 
-    wrappedTokenAddress = _wrappedTokenAddress;
-    originTokenAddress = _originTokenAddress;
-  }
+        wrappedTokenAddress = _wrappedTokenAddress;
+        originTokenAddress = _originTokenAddress;
+    }
 
-  /**
-   * @dev The WstETHBridgeL2 can only be upgraded by the admin
-   * @param v new WstETHBridgeL2 version
-   */
-  function _authorizeUpgrade(address v)
-    internal
-    override
-    onlyRole(DEFAULT_ADMIN_ROLE)
-  {}
+    /**
+     * @dev The WstETHBridgeL2 can only be upgraded by the admin
+     * @param v new WstETHBridgeL2 version
+     */
+    function _authorizeUpgrade(
+        address v
+    ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 
-  /**
-   * @notice Pause the bridge
-   * @dev Only EMERGENCY_ROLE can pause the bridge
-   */
-  function pause() external virtual onlyRole(EMERGENCY_ROLE) {
-    _pause();
-  }
+    /**
+     * @notice Pause the bridge
+     * @dev Only EMERGENCY_ROLE can pause the bridge
+     */
+    function pause() external virtual onlyRole(EMERGENCY_ROLE) {
+        _pause();
+    }
 
-  /**
-   * @notice Resume the bridge
-   * @dev Only EMERGENCY_ROLE can resume the bridge
-   */
-  function unpause() external virtual onlyRole(EMERGENCY_ROLE) {
-    _unpause();
-  }
+    /**
+     * @notice Resume the bridge
+     * @dev Only EMERGENCY_ROLE can resume the bridge
+     */
+    function unpause() external virtual onlyRole(EMERGENCY_ROLE) {
+        _unpause();
+    }
 
-  /**
-   * @dev Handle the reception of the tokens
-   * @param amount Token amount
-   */
-  function _receiveTokens(uint256 amount)
-    internal
-    virtual
-    override
-    whenNotPaused
-  {
-    wrappedTokenAddress.bridgeBurn(msg.sender, amount);
-  }
+    /**
+     * @dev Handle the reception of the tokens
+     * @param amount Token amount
+     */
+    function _receiveTokens(
+        uint256 amount
+    ) internal virtual override whenNotPaused {
+        wrappedTokenAddress.bridgeBurn(msg.sender, amount);
+    }
 
-  /**
-   * @dev Handle the transfer of the tokens
-   * @param destinationAddress Address destination that will receive the tokens
-   * on the other network
-   * @param amount Token amount
-   */
-  function _transferTokens(address destinationAddress, uint256 amount)
-    internal
-    virtual
-    override
-    whenNotPaused
-  {
-    wrappedTokenAddress.bridgeMint(destinationAddress, amount);
-  }
+    /**
+     * @dev Handle the transfer of the tokens
+     * @param destinationAddress Address destination that will receive the tokens
+     * on the other network
+     * @param amount Token amount
+     */
+    function _transferTokens(
+        address destinationAddress,
+        uint256 amount
+    ) internal virtual override whenNotPaused {
+        wrappedTokenAddress.bridgeMint(destinationAddress, amount);
+    }
 }
--- test/WstETHWrapped.t.sol
+++ test/WstETHWrapped.t.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.17;
 
 import {Test} from "forge-std/Test.sol";
 
+import {CREATE3Factory} from "../src/helper/Create3Factory.sol";
 import {ICREATE3Factory} from "../src/interfaces/ICREATE3Factory.sol";
 import {WstETHWrapped} from "../src/WstETHWrapped.sol";
-import {WstETHWrappedUUPSProxy} from
-  "../src/proxies/WstETHWrappedUUPSProxy.sol";
+import {WstETHWrappedUUPSProxy} from "../src/proxies/WstETHWrappedUUPSProxy.sol";
 
 /**
  * @title WstETHV2Mock
@@ -14,17 +14,17 @@ import {WstETHWrappedUUPSProxy} from
  * @notice Mock contract to test upgradeability of WstETH smart contract
  */
 contract WstETHWrappedV2Mock is WstETHWrapped {
-  uint256 public some;
+    uint256 public some;
 
-  /// @dev Add new function for testing purpose
-  function setValue(uint256 _some) public {
-    some = _some;
-  }
+    /// @dev Add new function for testing purpose
+    function setValue(uint256 _some) public {
+        some = _some;
+    }
 
-  /// @dev Add new function for testing purpose
-  function getValue() public view returns (uint256 b) {
-    b = some;
-  }
+    /// @dev Add new function for testing purpose
+    function getValue() public view returns (uint256 b) {
+        b = some;
+    }
 }
 
 /**
@@ -33,158 +33,169 @@ contract WstETHWrappedV2Mock is WstETHWrapped {
  * @notice Unit tests for WstETHWrapped
  */
 contract WstETHWrappedTest is Test {
-  string ZKEVM_RPC_URL = vm.envString("ZKEVM_RPC_URL");
+    string ZKEVM_RPC_URL = vm.envString("ZKEVM_RPC_URL");
 
-  ICREATE3Factory create3Factory =
-    ICREATE3Factory(0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1);
+    CREATE3Factory create3Factory;
 
-  address deployer = vm.addr(0xC14C13);
-  address admin = vm.addr(0xB453D);
-  address emergency = vm.addr(0xD4DD1);
-  address alice = vm.addr(0xA11CE);
-  address bob = vm.addr(0xB0B);
-  address wstETHBridgeAddress;
+    address deployer = vm.addr(0xC14C13);
+    address admin = vm.addr(0xB453D);
+    address emergency = vm.addr(0xD4DD1);
+    address alice = vm.addr(0xA11CE);
+    address bob = vm.addr(0xB0B);
+    address wstETHBridgeAddress;
 
-  WstETHWrapped wrappedToken;
+    WstETHWrapped wrappedToken;
 
-  function _getWstETHBridgeL2Address() internal returns (address) {
-    return
-      create3Factory.getDeployed(deployer, keccak256(bytes("WstETHBridgeL2")));
-  }
+    function _getWstETHBridgeL2Address() internal returns (address) {
+        return
+            create3Factory.getDeployed(
+                deployer,
+                keccak256(bytes("WstETHBridgeL2"))
+            );
+    }
 
-  function _deployWstETHWrapped() internal returns (WstETHWrapped token) {
-    vm.startPrank(deployer);
+    function _deployWstETHWrapped() internal returns (WstETHWrapped token) {
+        vm.startPrank(deployer);
 
-    wstETHBridgeAddress = _getWstETHBridgeL2Address();
-    WstETHWrapped implementation = new WstETHWrapped();
-    bytes memory data = abi.encodeWithSelector(
-      WstETHWrapped.initialize.selector, admin, emergency, wstETHBridgeAddress
-    );
-    bytes32 salt = keccak256(bytes("WstETHWrapped"));
-    bytes memory creationCode = abi.encodePacked(
-      type(WstETHWrappedUUPSProxy).creationCode,
-      abi.encode(address(implementation), data)
-    );
-    address deployedAddress = create3Factory.deploy(salt, creationCode);
-    token = WstETHWrapped(deployedAddress);
+        create3Factory = new CREATE3Factory();
+        wstETHBridgeAddress = _getWstETHBridgeL2Address();
+        WstETHWrapped implementation = new WstETHWrapped();
+        bytes memory data = abi.encodeWithSelector(
+            WstETHWrapped.initialize.selector,
+            admin,
+            emergency,
+            wstETHBridgeAddress
+        );
+        bytes32 salt = keccak256(bytes("WstETHWrapped"));
+        bytes memory creationCode = abi.encodePacked(
+            type(WstETHWrappedUUPSProxy).creationCode,
+            abi.encode(address(implementation), data)
+        );
+        address deployedAddress = create3Factory.deploy(salt, creationCode);
+        token = WstETHWrapped(deployedAddress);
 
-    vm.stopPrank();
-  }
+        vm.stopPrank();
+    }
 
-  function setUp() public {
-    uint256 zkEvmFork = vm.createFork(ZKEVM_RPC_URL);
-    vm.selectFork(zkEvmFork);
+    function setUp() public {
+        uint256 zkEvmFork = vm.createFork(ZKEVM_RPC_URL);
+        vm.selectFork(zkEvmFork);
 
-    wrappedToken = _deployWstETHWrapped();
-  }
+        wrappedToken = _deployWstETHWrapped();
+    }
 
-  // ==========================================================================
-  // == Upgradeability ========================================================
-  // ==========================================================================
+    // ==========================================================================
+    // == Upgradeability ========================================================
+    // ==========================================================================
 
-  /// @notice Upgrade as admin; make sure it works as expected
-  function testUpgradeAsAdmin() public {
-    // Deploy new implementation
-    WstETHWrappedV2Mock v2 = new WstETHWrappedV2Mock();
+    /// @notice Upgrade as admin; make sure it works as expected
+    function testUpgradeAsAdmin() public {
+        // Deploy new implementation
+        WstETHWrappedV2Mock v2 = new WstETHWrappedV2Mock();
 
-    vm.startPrank(admin);
-    wrappedToken.upgradeTo(address(v2));
-    vm.stopPrank();
+        vm.startPrank(admin);
+        wrappedToken.upgradeTo(address(v2));
+        vm.stopPrank();
 
-    WstETHWrappedV2Mock wrappedTokenV2 =
-      WstETHWrappedV2Mock(address(wrappedToken));
-    wrappedTokenV2.setValue(2);
-    assertEq(wrappedTokenV2.getValue(), 2);
-  }
+        WstETHWrappedV2Mock wrappedTokenV2 = WstETHWrappedV2Mock(
+            address(wrappedToken)
+        );
+        wrappedTokenV2.setValue(2);
+        assertEq(wrappedTokenV2.getValue(), 2);
+    }
 
-  /// @notice Upgrade as non-admin; make sure it reverted
-  function testUpgradeAsNonAdmin() public {
-    vm.startPrank(alice);
-    vm.expectRevert(
-      bytes(
-        "AccessControl: account 0xe05fcc23807536bee418f142d19fa0d21bb0cff7 is missing role 0x0000000000000000000000000000000000000000000000000000000000000000"
-      )
-    );
-    wrappedToken.upgradeTo(vm.addr(2));
-  }
+    /// @notice Upgrade as non-admin; make sure it reverted
+    function testUpgradeAsNonAdmin() public {
+        vm.startPrank(alice);
+        vm.expectRevert(
+            bytes(
+                "AccessControl: account 0xe05fcc23807536bee418f142d19fa0d21bb0cff7 is missing role 0x0000000000000000000000000000000000000000000000000000000000000000"
+            )
+        );
+        wrappedToken.upgradeTo(vm.addr(2));
+    }
 
-  // ==========================================================================
-  // == Pausability ===========================================================
-  // ==========================================================================
+    // ==========================================================================
+    // == Pausability ===========================================================
+    // ==========================================================================
 
-  /// @notice Pause as emergency role; make sure it works as expected
-  function testPauseAsEmergencyRole() public {
-    vm.startPrank(emergency);
-    wrappedToken.pause();
-    vm.stopPrank();
+    /// @notice Pause as emergency role; make sure it works as expected
+    function testPauseAsEmergencyRole() public {
+        vm.startPrank(emergency);
+        wrappedToken.pause();
+        vm.stopPrank();
 
-    deal(address(wrappedToken), alice, 10 ether);
-    vm.startPrank(alice);
-    vm.expectRevert("Pausable: paused");
-    wrappedToken.transfer(bob, 10 ether);
+        deal(address(wrappedToken), alice, 10 ether);
+        vm.startPrank(alice);
+        vm.expectRevert("Pausable: paused");
+        wrappedToken.transfer(bob, 10 ether);
 
-    assertTrue(wrappedToken.paused());
-  }
+        assertTrue(wrappedToken.paused());
+    }
 
-  /// @notice Pause as non-emergency role; make sure it reverted
-  function testPauseAsNonEmergencyRole() public {
-    vm.startPrank(alice);
-    vm.expectRevert(
-      bytes(
-        "AccessControl: account 0xe05fcc23807536bee418f142d19fa0d21bb0cff7 is missing role 0xbf233dd2aafeb4d50879c4aa5c81e96d92f6e6945c906a58f9f2d1c1631b4b26"
-      )
-    );
-    wrappedToken.pause();
-  }
+    /// @notice Pause as non-emergency role; make sure it reverted
+    function testPauseAsNonEmergencyRole() public {
+        vm.startPrank(alice);
+        vm.expectRevert(
+            bytes(
+                "AccessControl: account 0xe05fcc23807536bee418f142d19fa0d21bb0cff7 is missing role 0xbf233dd2aafeb4d50879c4aa5c81e96d92f6e6945c906a58f9f2d1c1631b4b26"
+            )
+        );
+        wrappedToken.pause();
+    }
 
-  // ==========================================================================
-  // == ERC-2612 ==============================================================
-  // ==========================================================================
+    // ==========================================================================
+    // == ERC-2612 ==============================================================
+    // ==========================================================================
 
-  /// @notice Make sure it support ERC-2612
-  function testERC2612Compliant() public {
-    assertEq(wrappedToken.nonces(alice), 0);
-    assertTrue(wrappedToken.DOMAIN_SEPARATOR() != "");
-  }
+    /// @notice Make sure it support ERC-2612
+    function testERC2612Compliant() public {
+        assertEq(wrappedToken.nonces(alice), 0);
+        assertTrue(wrappedToken.DOMAIN_SEPARATOR() != "");
+    }
 
-  // ==========================================================================
-  // == Mint ==================================================================
-  // ==========================================================================
+    // ==========================================================================
+    // == Mint ==================================================================
+    // ==========================================================================
 
-  /// @notice Mint as bridge
-  function testBridgeMintAsBridge() public {
-    vm.startPrank(wstETHBridgeAddress);
-    wrappedToken.bridgeMint(alice, 1 ether);
-    vm.stopPrank();
+    /// @notice Mint as bridge
+    function testBridgeMintAsBridge() public {
+        vm.startPrank(wstETHBridgeAddress);
+        wrappedToken.bridgeMint(alice, 1 ether);
+        vm.stopPrank();
 
-    assertEq(wrappedToken.balanceOf(alice), 1 ether);
-  }
+        assertEq(wrappedToken.balanceOf(alice), 1 ether);
+    }
 
-  /// @notice Mint as non bridge
-  function testBridgeMintAsNonBridge() public {
-    vm.startPrank(alice);
-    vm.expectRevert("CustomERC20Wrapped::onlyBridge: Not PolygonZkEVMBridge");
-    wrappedToken.bridgeMint(alice, 1 ether);
-  }
+    /// @notice Mint as non bridge
+    function testBridgeMintAsNonBridge() public {
+        vm.startPrank(alice);
+        vm.expectRevert(
+            "CustomERC20Wrapped::onlyBridge: Not PolygonZkEVMBridge"
+        );
+        wrappedToken.bridgeMint(alice, 1 ether);
+    }
 
-  // ==========================================================================
-  // == Burn ==================================================================
-  // ==========================================================================
+    // ==========================================================================
+    // == Burn ==================================================================
+    // ==========================================================================
 
-  /// @notice Burn as bridge
-  function testBridgeBurnAsBridge() public {
-    vm.startPrank(wstETHBridgeAddress);
-    wrappedToken.bridgeMint(alice, 1 ether);
-    wrappedToken.bridgeBurn(alice, 1 ether);
-    vm.stopPrank();
+    /// @notice Burn as bridge
+    function testBridgeBurnAsBridge() public {
+        vm.startPrank(wstETHBridgeAddress);
+        wrappedToken.bridgeMint(alice, 1 ether);
+        wrappedToken.bridgeBurn(alice, 1 ether);
+        vm.stopPrank();
 
-    assertEq(wrappedToken.balanceOf(alice), 0);
-  }
+        assertEq(wrappedToken.balanceOf(alice), 0);
+    }
 
-  /// @notice Burn as non bridge
-  function testBridgeBurnAsNonBridge() public {
-    vm.startPrank(alice);
-    vm.expectRevert("CustomERC20Wrapped::onlyBridge: Not PolygonZkEVMBridge");
-    wrappedToken.bridgeBurn(alice, 1 ether);
-  }
+    /// @notice Burn as non bridge
+    function testBridgeBurnAsNonBridge() public {
+        vm.startPrank(alice);
+        vm.expectRevert(
+            "CustomERC20Wrapped::onlyBridge: Not PolygonZkEVMBridge"
+        );
+        wrappedToken.bridgeBurn(alice, 1 ether);
+    }
 }
